f395f4040fe0e06511aed565899c7245747d4a1e
Igor Lira <igorlira@live.com>
2019-03-09 17:25:32 -0800

    Initial Commit

---
+++ Relay/Relay.h
@@ -0,0 +1,13 @@
+//
+//  Relay.h
+//  Relay
+//
+//  Created by Igor Lira on 3/9/19.
+//  Copyright © 2019 Igor Lira. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Relay : NSObject
+
+@end
---
+++ Relay/Relay.m
@@ -0,0 +1,13 @@
+//
+//  Relay.m
+//  Relay
+//
+//  Created by Igor Lira on 3/9/19.
+//  Copyright © 2019 Igor Lira. All rights reserved.
+//
+
+#import "Relay.h"
+
+@implementation Relay
+
+@end
